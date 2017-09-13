d81ce49e2a03c046337aa5df7eca909320e70ade
TejasviRaut <31839953+TejasviRaut@users.noreply.github.com>
2017-09-13 14:28:38 +0530

    Function declaration
    classes object and methods in ruby

---
+++ Sample.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+# class declaration
+class Sample
+	# function declaration using def keyword
+	def hello
+		puts "Hello Ruby!"
+	end
+end
+
+# creating instance of above class
+object = Sample. new
+
+# calling method of above class using object
+object.hello
+
+
+		
