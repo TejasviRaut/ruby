2e1a367e4d7416324739b5c58369d0156ebdcece
TejasviRaut <31839953+TejasviRaut@users.noreply.github.com>
2017-09-13 12:59:01 +0530

    Comments in ruby
    go through the code

---
+++ comments.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+puts "Comments aint visible! check the code"
+
+# a single line comment in ruby
+# comments aint visible in the output
+# used to provide info about code
+# well, i dont want to use single line comment 
+# i need multiple line comments.
+
+=begin
+Lucky you, 
+this is a multiple line comment in ruby
+used when you have a long description,
+about the code functionality
+=end
