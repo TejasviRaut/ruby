b5ee42e60b2c5bf5ea2de4ba4714e88f615182dd
TejasviRaut <31839953+TejasviRaut@users.noreply.github.com>
2017-09-13 16:44:29 +0530

    Expression evaluation

---
+++ expr.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby -w
+puts "Multiplication Value of 24 and 6 : #{24 * 6}";
+puts "Addition Value of 24 and 6 : #{24 + 6}";
+puts "Subtraction Value of 24 and 6 : #{24 - 6}";
+puts "Division Value of 24 and 6 : #{24 / 6}";
+puts "Modulus Value of 24 and 6 : #{24 % 6}";
