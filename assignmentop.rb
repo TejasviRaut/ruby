aa14a340798b1f2a857ddf0e3377572fa78c4222
TejasviRaut <31839953+TejasviRaut@users.noreply.github.com>
2017-09-14 12:28:14 +0530

    assignment operator
    parallel assignment and swapping using parallel assignment

---
+++ assignmentop.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+a = 10
+b = 20
+c = a+b
+print "\na: " , a , " b: " , b , " c: " , c 
+
+# parallel assignment
+
+a, b, c = 10, 20, 30
+puts "\nParallel assignment"
+print "a: " , a , " b: " , b , " c: " , c 
+
+# parallel assignment used for swapping
+
+a, b, c = b, c, a
+puts "\nSwapping using Parallel assignment"
+print "a: " , a , " b: " , b , " c: " , c 
+
+
+
