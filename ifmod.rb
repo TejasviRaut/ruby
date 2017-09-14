ea1f5994441bdd09a9a7be32ea139ef1f0de1fe4
TejasviRaut <31839953+TejasviRaut@users.noreply.github.com>
2017-09-14 14:33:44 +0530

    if and unless modifier

---
+++ ifmod.rb
@@ -0,0 +1,6 @@
+$debug = 1
+a = 10
+print "debug\n" if $debug 	# print if the condition is true
+print "foo\n" if $foo	
+print "greater " if a > 0 
+print "less" unless a > 20	# print if the condition is false
