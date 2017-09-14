b64f6a1d27f61e1f13bcadf039acb7a61b057324
TejasviRaut <31839953+TejasviRaut@users.noreply.github.com>
2017-09-14 18:31:45 +0530

    Return statement
    every function in ruby returns a value by default it is the value of the last line of the function

---
+++ return.rb
@@ -0,0 +1,21 @@
+def add(a, b)
+	sum = a + b # default: returns the last line of any function 
+	# every function in ruby returns a value
+end
+def sub(a,b)
+	minus = a - b
+	return 
+end
+def product(a,b)
+	prod = a * b
+	return a, b, prod
+end
+
+var1 = add(5,10)
+puts "Add: #{var1}"
+
+var1 = sub(5,10)
+puts "Subtract: #{var1}"
+
+var1 = product(5,10)
+puts "product: #{var1}"
