3390009729f21761a7f3bacea748070e6bc645c3
TejasviRaut <31839953+TejasviRaut@users.noreply.github.com>
2017-09-14 14:44:57 +0530

    case .. when
    switch case in ruby

---
+++ switch.rb
@@ -0,0 +1,31 @@
+$day = 4
+case $day
+when 1
+	puts "Sunday"
+when 2
+	puts "Monday"
+when 3
+	puts "Tuesday"
+when 4
+	puts "Wednesday"
+when 5
+	puts "Thursday"
+when 6
+	puts "Friday"
+when 7
+	puts "Saturday"
+end
+
+$age = 24
+case $age
+when 0..2
+	puts "baby"
+when 3..12
+	puts "child"
+when 13..18
+	puts "teenager"
+when 19..59
+	puts "adult"
+when 60..100
+	puts "oldage"
+end
