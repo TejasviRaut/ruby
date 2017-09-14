b823aae695cae7e36a94c4be63dcd369263501f4
TejasviRaut <31839953+TejasviRaut@users.noreply.github.com>
2017-09-14 12:29:23 +0530

    bitwise operator

---
+++ bitwiseop.rb
@@ -0,0 +1,11 @@
+a = 60
+b = 13
+
+print "\na: " , a , " b: " , b , "\n"
+print "a&b: " , a&b , "\n"
+print "a|b: " , a|b , "\n"
+print "a^b: " , a^b , "\n"
+print "~a: " , ~a , "\n"
+print "a<<2: " , a<<2 , "\n"
+print "a>>2: " , a>>2 , "\n"
+
