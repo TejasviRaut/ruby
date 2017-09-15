67158e29ab3ebb2237292d1eea3bd25ef3b4d0fa
TejasviRaut <31839953+TejasviRaut@users.noreply.github.com>
2017-09-15 13:03:16 +0530

    blocks
    yield and block.call

--- block.rb
+++ block.rb
@@ -5,6 +5,13 @@ def test
 	yield
 	puts "you are again back to the method"
 	yield
+	puts "you are again back to the method"
 end
 test {puts "you are in the block"}
 
+
+def test1(&block)
+	block.call
+end
+
+test1 {puts "hello, welcome to the block"}
