08bafdb6e2cc950d758613ce8e08b8b479006107
TejasviRaut <31839953+TejasviRaut@users.noreply.github.com>
2017-09-15 15:15:28 +0530

    mixins
    include statement in class

---
+++ week.rb
@@ -0,0 +1,12 @@
+module Week 
+	FIRST_DAY = "Sunday"
+	NO_OF_DAYS = 7
+
+	def Week.weeks_in_month
+		puts "4 weeks in a month"
+	end
+
+	def Week.weeks_in_year
+		puts "52 weeks in a year"
+	end
+end
