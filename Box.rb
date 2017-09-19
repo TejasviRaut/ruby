c622ed6fc129a315e6596035735006fbe45d663b
TejasviRaut <31839953+TejasviRaut@users.noreply.github.com>
2017-09-19 11:37:32 +0530

    inheritance method overriding

--- Box.rb
+++ Box.rb
@@ -2,10 +2,20 @@
 class Box
 	@@area = 0
 	@@volume = 0
+	@@count = 0
 
 	# constructor method
 	def initialize(l,h,b)
 		@length, @height, @breadth= l, h, b
+		@@count +=1 
+	end
+
+	def self.printCount()
+		puts "Box count is : #@@count"
+	end
+
+	def to_s
+		"(l:#@length, b:#@breadth, h:#@height)"
 	end
 
 	#accessor metods
@@ -55,22 +65,57 @@ class Box
 		calArea
 		calVolume
 	end
+	#protected :displayDetails
 	
 	def calArea
 		@@area = 2 * ((@length * @breadth ) + (@breadth * @height) + (@length * @height))
-		puts "Area: #@@area units^2"
+		puts "Area: #@@area sq. units"
 	end
 
 	def calVolume
 		@@volume = @length * @breadth * @height
-		puts "Volume: #@@volume units^3"
+		puts "Volume: #@@volume cu. units"
+	end
+	protected :calVolume
+	
+end
+
+class BigBox < Box
+	
+	def calArea
+		@@area = 2 * ((@length * @breadth ) + (@breadth * @height) + (@length * @height))
+		puts "Area of Big Box: #@@area sq. units"
 	end
 end
 
 box = Box.new(10, 5, 4)
+box1 = Box.new(10, 20, 2)
+box2 = Box.new(30, 100, 1)
+box3 = Box.allocate
+
+box.freeze
+if( box.frozen? )
+   puts "Box object is frozen object"
+else
+   puts "Box object is normal object"
+end
+#box.setLength
+
 box.displayDetails
-box.setLength
-box.setHeight
-box.setBreadth
-box.displayDetails
+box1.displayDetails
+box2.displayDetails
+
+
+box3.setLength
+box3.setHeight
+box3.setBreadth
+box3.displayDetails
+
+Box.printCount()
+
+puts "String rep of box: #{box}"
+
+bigbox = BigBox.new(20,20,20)
+bigbox.calArea()
+bigbox.displayDetails
 
