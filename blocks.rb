16436e52d2e82f4da460f8b418fcb20af215d65d
TejasviRaut <31839953+TejasviRaut@users.noreply.github.com>
2017-09-23 12:54:34 +0530

    Blocks

---
+++ blocks.rb
@@ -0,0 +1,15 @@
+def produce 
+	yield :spaceship, :freighter, :yacht, size: :s, seats: :leather, cargo: :cargo
+end
+
+produce do |what = :spaceship, *types,  size: :m, engine_count:2, **custom_components|
+	puts "Producing #{what}"
+	print "Types : "
+	p types
+	puts "Size: #{size}"
+	puts "Engine count: #{engine_count} "
+	print "Custom components: "
+	p custom_components
+end
+
+
